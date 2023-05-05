c8a48a4d541cf15d96af504bb0d6c74768dc8dc1
mkv <kons@devake.com>
2023-05-05 20:33:45 +0600

    HEX Colors fix.

--- Sources/AlpineCore/Extensions/Color.swift
+++ Sources/AlpineCore/Extensions/Color.swift
@@ -5,75 +5,57 @@
 //  Created by Jenya Lebid on 5/3/23.
 //
 
-import SwiftUI
-
-public extension Color {
-    
+//import SwiftUI
+//
+//public extension Color {
+//    // alpha is in last place
+////    public init(hexString: String) {
+////        let scanner = Scanner(string: hexString)
+////        var hexValue: UInt64 = 0
+////        if scanner.scanHexInt64(&hexValue) {
+////            let r = CGFloat((hexValue & 0xFF000000) >> 24) / 255.0
+////            let g = CGFloat((hexValue & 0x00FF0000) >> 16) / 255.0
+////            let b = CGFloat((hexValue & 0x0000FF00) >> 8) / 255.0
+////            let a = CGFloat(hexValue & 0x000000FF) / 255.0
+////
+////            self.init(red: r, green: g, blue: b, opacity: a)
+////        } else {
+////            self.init(.black)
+////        }
+////    }
+//
+//    // alpha is in first place
 //    init(hex: String) {
-//           let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
-//           var int: UInt64 = 0
-//           Scanner(string: hex).scanHexInt64(&int)
-//           let a, r, g, b: UInt64
-//           switch hex.count {
-//               case 3: // RGB (12-bit)
-//                   (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
-//               case 6: // RGB (24-bit)
-//                   (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
-//               case 8: // ARGB (32-bit)
-//                   (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
-//               default:
-//                   (a, r, g, b) = (255, 0, 0, 0)
-//           }
-//           self.init(.sRGB, red: Double(r) / 255, green: Double(g) / 255, blue: Double(b) / 255, opacity: Double(a) / 255)
-//       }
-    
-    init(hex: String) {
-        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
-        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
-
-        var rgb: UInt64 = 0
-
-        var r: CGFloat = 0.0
-        var g: CGFloat = 0.0
-        var b: CGFloat = 0.0
-        var a: CGFloat = 1.0
-
-        let length = hexSanitized.count
-
-        guard Scanner(string: hexSanitized).scanHexInt64(&rgb) else {
-            self.init(CGColor.init(red: 0, green: 0, blue: 0, alpha: 1))
-            return
-        }
-
-        if length == 6 {
-            r = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
-            g = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
-            b = CGFloat(rgb & 0x0000FF) / 255.0
-
-        } else if length == 8 {
-            r = CGFloat((rgb & 0xFF000000) >> 24) / 255.0
-            g = CGFloat((rgb & 0x00FF0000) >> 16) / 255.0
-            b = CGFloat((rgb & 0x0000FF00) >> 8) / 255.0
-            a = CGFloat(rgb & 0x000000FF) / 255.0
-
-        } else {
-            self.init(CGColor.init(red: 0, green: 0, blue: 0, alpha: 1))
-        }
-
-        self.init(red: r, green: g, blue: b, opacity: a)
-    }
-    
-    func toHex(includeAlpha: Bool = true) -> String {
-        let components = self.cgColor?.components ?? [0, 0, 0, 0]
-        let r = components[0]
-        let g = components[1]
-        let b = components[2]
-        let a = components.count >= 4 ? components[3] : 1.0
-
-        if includeAlpha {
-            return String(format: "#%02lX%02lX%02lX%02lX", lroundf(Float(r * 255)), lroundf(Float(g * 255)), lroundf(Float(b * 255)), lroundf(Float(a * 255)))
-        } else {
-            return String(format: "#%02lX%02lX%02lX", lroundf(Float(r * 255)), lroundf(Float(g * 255)), lroundf(Float(b * 255)))
-        }
-    }
-}
+//        var hexSanitized: String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+//        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+//        if (hexSanitized.count != 6 && hexSanitized.count != 8) {
+//            self.init(CGColor.init(red: 0, green: 0, blue: 0, alpha: 1))
+//            return
+//        }
+//        var rgbValue: UInt64 = 0
+//        Scanner(string: hexSanitized).scanHexInt64(&rgbValue)
+//        let a: CGFloat = hexSanitized.count == 6 ? 1.0 : CGFloat((rgbValue & 0xFF000000) >> 24) / 255.0
+//        let r: CGFloat = CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0
+//        let g: CGFloat = CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0
+//        let b: CGFloat = CGFloat( rgbValue & 0x0000FF) / 255.0
+//
+//        self.init(red: r, green: g, blue: b, opacity: a)
+//    }
+//
+//    func toHex() -> String {
+//        let uic = UIColor(self)
+//        guard let components = uic.cgColor.components, (components.count == 3 || components.count == 4) else {
+//            return "#ff000000"
+//        }
+//        let r = Float(components[0])
+//        let g = Float(components[1])
+//        let b = Float(components[2])
+//        let a = components.count == 4 ? Float(components[3]) : 1.0
+//
+//        if a != Float(1.0) {
+//            return String(format: "#%02lX%02lX%02lX%02lX", lroundf(a * 255), lroundf(r * 255), lroundf(g * 255), lroundf(b * 255))
+//        } else {
+//            return String(format: "#%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255))
+//        }
+//    }
+//}
