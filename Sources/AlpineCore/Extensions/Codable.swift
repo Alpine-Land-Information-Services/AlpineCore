68a6473b30b8e4a24e90ad116acd005558580fa7
Jenya Lebid <61124137+jenyalebid@users.noreply.github.com>
2023-05-12 15:54:25 -0700

    Update Codable.swift

--- Sources/AlpineCore/Extensions/Codable.swift
+++ Sources/AlpineCore/Extensions/Codable.swift
@@ -23,4 +23,13 @@ public extension Encodable {
     }
 }
 
+public extension KeyedEncodingContainer {
+    
+    mutating func encodeIfNotDefault<T: Encodable & Equatable>(_ value: T?, forKey key: KeyedEncodingContainer<K>.Key, defaultValue: T) throws {
+        if let value = value, value != defaultValue {
+            try encode(value, forKey: key)
+        }
+    }
+}
+
 
