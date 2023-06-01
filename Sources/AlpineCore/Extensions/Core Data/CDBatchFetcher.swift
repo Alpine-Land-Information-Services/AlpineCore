466141a23afae40a335d675bfc9ded49d468c2cb
Jenya Lebid <61124137+jenyalebid@users.noreply.github.com>
2023-06-01 13:11:06 -0700

    New export complete.

--- Sources/AlpineCore/Extensions/Core Data/CDBatchFetcher.swift
+++ Sources/AlpineCore/Extensions/Core Data/CDBatchFetcher.swift
@@ -8,25 +8,68 @@
 import CoreData
 
 public class CDBatchFetcher {
-    
+
     private let fetchRequest: NSFetchRequest<NSManagedObject>
 
     private var currentBatch = 0
+    private var isModifying: Bool
 
-    public init(for entityName: String, using predicate: NSPredicate?, with batchSize: Int) {
+    public init(for entityName: String, using predicate: NSPredicate?, with batchSize: Int, isModifying: Bool) {
         fetchRequest = NSFetchRequest<NSManagedObject>(entityName: entityName)
         fetchRequest.predicate = predicate
         fetchRequest.fetchBatchSize = batchSize
         fetchRequest.fetchLimit = batchSize
-        fetchRequest.returnsObjectsAsFaults = false
+        
+        self.isModifying = isModifying
     }
 
     public func fetchObjectBatch(in context: NSManagedObjectContext) throws -> [NSManagedObject]? {
         fetchRequest.fetchOffset = currentBatch * fetchRequest.fetchLimit
 
         let results = try context.fetch(fetchRequest)
+
+        if !isModifying {
+            currentBatch += 1
+        }
         
-        currentBatch += 1
         return results.isEmpty ? nil : results
     }
 }
+
+
+//public class CDBatchFetcher {
+//
+//    private let entityName: String
+//    private let predicate: NSPredicate?
+//    private let batchSize: Int
+//    private var currentBatch = 0
+//
+//    public init(for entityName: String, using predicate: NSPredicate?, with batchSize: Int) {
+//        self.entityName = entityName
+//        self.predicate = predicate
+//        self.batchSize = batchSize
+//    }
+//
+//    public func fetchObjectBatch(in context: NSManagedObjectContext) throws -> [NSManagedObject]? {
+//        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: self.entityName)
+//        fetchRequest.predicate = self.predicate
+//        fetchRequest.fetchLimit = self.batchSize
+//        fetchRequest.fetchOffset = self.currentBatch * self.batchSize
+//        fetchRequest.returnsObjectsAsFaults = false
+//
+//        let results = try context.fetch(fetchRequest)
+//
+//        if results.isEmpty {
+//            return nil
+//        } else {
+//            self.currentBatch += 1
+//            return results
+//        }
+//    }
+//}
+
+
+
+
+
+
