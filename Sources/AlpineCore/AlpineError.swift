fa2c3d0a90128ad33744e9c66a132ae6562a862b
Jenya Lebid <61124137+jenyalebid@users.noreply.github.com>
2023-10-11 15:54:34 -0700

    Create AlpineError.swift

---
+++ Sources/AlpineCore/AlpineError.swift
@@ -0,0 +1,23 @@
+//
+//  AlpineError.swift
+//  AlpineCore
+//
+//  Created by Jenya Lebid on 10/11/23.
+//
+
+import Foundation
+
+open class AlpineError: Error {
+    
+    public var message: String
+    public var file: String
+    public var function: String
+    public var line: Int
+
+    public init(_ message: String, file: String = #file, function: String = #function, line: Int = #line) {
+        self.message = message
+        self.file = file
+        self.function = function
+        self.line = line
+    }
+}
