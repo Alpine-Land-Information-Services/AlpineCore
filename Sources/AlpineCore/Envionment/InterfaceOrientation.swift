a604240e20bc443cb81aaece4912ef9a9b26237f
Jenya Lebid <61124137+jenyalebid@users.noreply.github.com>
2024-01-23 15:46:51 -0800

    Extensions added.

---
+++ Sources/AlpineCore/Envionment/InterfaceOrientation.swift
@@ -0,0 +1,68 @@
+//
+//  InterfaceOrientation.swift
+//  AlpineCore
+//
+//  Created by Jenya Lebid on 1/23/24.
+//
+
+import SwiftUI
+
+public enum UIOrientation {
+    case landcape
+    case portrait
+    case unknown
+    
+    public var isPortrait: Bool {
+        self == .portrait
+    }
+    
+    public var isLandscape: Bool {
+        self == .landcape
+    }
+}
+
+struct UIOrientationInfoKey: EnvironmentKey {
+    static let defaultValue: UIOrientation = .unknown
+}
+
+public extension EnvironmentValues {
+    var uiOrientation: UIOrientation {
+        get { self[UIOrientationInfoKey.self] }
+        set { self[UIOrientationInfoKey.self] = newValue }
+    }
+}
+
+struct InterfaceOrientation: ViewModifier {
+    
+    @State private var uiOrientation: UIOrientation = .unknown
+
+    func body(content: Content) -> some View {
+        GeometryReader { geometry in
+            content
+                .onAppear {
+                    getOrientation(from: geometry)
+                }
+                .onChange(of: geometry.size) { _, _ in
+                    getOrientation(from: geometry)
+                }
+                .environment(\.uiOrientation, uiOrientation)
+        }
+    }
+    
+    private func getOrientation(from geometry: GeometryProxy) {
+        if geometry.size.width > geometry.size.height {
+            uiOrientation = .landcape
+        }
+        else {
+            uiOrientation = .portrait
+        }
+    }
+}
+
+public extension View {
+    
+    var uiOrientationGetter: some View {
+        modifier(InterfaceOrientation())
+    }
+}
+
