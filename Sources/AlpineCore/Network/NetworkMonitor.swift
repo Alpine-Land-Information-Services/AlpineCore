4e4296b35de9b0b04fae388d7edbb9f04937ffed
Jenya Lebid <61124137+jenyalebid@users.noreply.github.com>
2024-02-09 15:58:31 -0800

    Update NetworkMonitor.swift

--- Sources/AlpineCore/Network/NetworkMonitor.swift
+++ Sources/AlpineCore/Network/NetworkMonitor.swift
@@ -8,55 +8,55 @@
 import Foundation
 import Network
 
-public class NetworkMonitor: ObservableObject {
-    
-    static public let shared = NetworkMonitor()
-    
-    public enum ConnectionType: String {
-        case offline
-        case wifi
-        case cellular
-    }
-    
-    @Published public var connectionType = ConnectionType.offline
-    @Published public var connected = false
-    
-    public func start() {
-        let monitor = NWPathMonitor()
-        monitor.start(queue: DispatchQueue(label: "NetworkMonitor"))
-        monitor.pathUpdateHandler = { path in
-            if path.status == .satisfied {
-                if path.isExpensive {
-                    self.changeConnectionType(.cellular)
-                }
-                else {
-                    self.changeConnectionType(.wifi)
-                }
-                DispatchQueue.main.async {
-                    self.connected = true
-                }
-            }
-            else {
-                DispatchQueue.main.async {
-                    self.connected = false
-                }
-                self.changeConnectionType(.offline)
-            }
-        }
-    }
-    
-    func changeConnectionType(_ type: ConnectionType) {
-        DispatchQueue.main.async {
-            self.connectionType = type
-        }
-    }
-    
-    public func checkConnection() -> Bool {
-        if connected {
-            return true
-        }
-//        AppControl.noConnectionAlert()
-        
-        return false
-    }
-}
+//public class NetworkMonitor: ObservableObject {
+//    
+//    static public let shared = NetworkMonitor()
+//    
+//    public enum ConnectionType: String {
+//        case offline
+//        case wifi
+//        case cellular
+//    }
+//    
+//    @Published public var connectionType = ConnectionType.offline
+//    @Published public var connected = false
+//    
+//    public func start() {
+//        let monitor = NWPathMonitor()
+//        monitor.start(queue: DispatchQueue(label: "NetworkMonitor"))
+//        monitor.pathUpdateHandler = { path in
+//            if path.status == .satisfied {
+//                if path.isExpensive {
+//                    self.changeConnectionType(.cellular)
+//                }
+//                else {
+//                    self.changeConnectionType(.wifi)
+//                }
+//                DispatchQueue.main.async {
+//                    self.connected = true
+//                }
+//            }
+//            else {
+//                DispatchQueue.main.async {
+//                    self.connected = false
+//                }
+//                self.changeConnectionType(.offline)
+//            }
+//        }
+//    }
+//    
+//    func changeConnectionType(_ type: ConnectionType) {
+//        DispatchQueue.main.async {
+//            self.connectionType = type
+//        }
+//    }
+//    
+//    public func checkConnection() -> Bool {
+//        if connected {
+//            return true
+//        }
+////        AppControl.noConnectionAlert()
+//        
+//        return false
+//    }
+//}
