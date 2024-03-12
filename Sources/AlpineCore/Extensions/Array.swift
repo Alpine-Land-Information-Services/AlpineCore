c6d0a443b52081e04ec119f10f6363d2e11b2f5b
mkv <kons@devake.com>
2024-03-12 15:34:49 +0500

    extension Array

--- Sources/AlpineCore/Extensions/Array.swift
+++ Sources/AlpineCore/Extensions/Array.swift
@@ -22,3 +22,15 @@ public extension Array where Element: Equatable {
         }
     }
 }
+
+
+public extension Array {
+    
+    mutating func move(from oldIndex: Index, to newIndex: Index) {
+        // Don't work for free and use swap when indices are next to each other - 
+        // this won't rebuild array and will be super efficient.
+        if oldIndex == newIndex { return }
+        if abs(newIndex - oldIndex) == 1 { return self.swapAt(oldIndex, newIndex) }
+        self.insert(self.remove(at: oldIndex), at: newIndex)
+    }
+}
