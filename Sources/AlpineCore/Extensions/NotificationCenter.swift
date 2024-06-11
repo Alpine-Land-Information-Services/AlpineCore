5657d1f11dc5013e5ef33ed25a477a8e525db47d
mkv <kons@devake.com>
2024-06-11 21:13:26 +0700

    DispatchQueue.main.async removed from post

--- Sources/AlpineCore/Extensions/NotificationCenter.swift
+++ Sources/AlpineCore/Extensions/NotificationCenter.swift
@@ -8,11 +8,9 @@
 import Foundation
 
 public extension NotificationCenter {
-    //TODO: change back to use without DispatchQueue.main.async - check function usage after it
+    
     static func post(_ name: NSNotification.Name, _ userInfo: [AnyHashable : Any]? = nil, _ object: Any? = nil) {
-        DispatchQueue.main.async {
-            NotificationCenter.default.post(name: name, object: object, userInfo: userInfo)
-        }
+        NotificationCenter.default.post(name: name, object: object, userInfo: userInfo)
     }
     
     static func postMainAsync(_ name: NSNotification.Name, _ userInfo: [AnyHashable : Any]? = nil, _ object: Any? = nil) {
