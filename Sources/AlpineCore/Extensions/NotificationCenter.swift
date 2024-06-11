53929eb243cf6650e24e207a7f0e21c1a8bdc6ea
mkv <kons@devake.com>
2024-06-11 11:23:02 +0700

    reminder

--- Sources/AlpineCore/Extensions/NotificationCenter.swift
+++ Sources/AlpineCore/Extensions/NotificationCenter.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public extension NotificationCenter {
-    
+    //TODO: change back to use without DispatchQueue.main.async - check function usage after it
     static func post(_ name: NSNotification.Name, _ userInfo: [AnyHashable : Any]? = nil, _ object: Any? = nil) {
         DispatchQueue.main.async {
             NotificationCenter.default.post(name: name, object: object, userInfo: userInfo)
