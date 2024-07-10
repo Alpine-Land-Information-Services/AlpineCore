2ba1f19df3a1033c6f6d2d55069f31be52040f2c
Murygin Vladislav <muriginvlad@gmail.com>
2024-07-09 22:23:39 -0500

    #170

--- Sources/AlpineCore/Extensions/String.swift
+++ Sources/AlpineCore/Extensions/String.swift
@@ -9,14 +9,8 @@ import Foundation
 
 public extension String {
     
-    var separated: String {
-        self
-            .replacingOccurrences(of: "([A-Z])", with: " $1", options: .regularExpression, range: range(of: self))
-            .trimmingCharacters(in: .whitespacesAndNewlines)
-            .capitalized
-    }
-    
     var fsPath: FSPath {
         FSPath(rawValue: self)
     }
+    
 }
