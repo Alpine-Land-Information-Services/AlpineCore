75ba4b604f3c30f2e3ee72acfaf1e71b2c109758
Jenya Lebid <61124137+jenyalebid@users.noreply.github.com>
2024-08-12 15:54:54 -0700

    Create View.swift

---
+++ Sources/AlpineCore/Extensions/View.swift
@@ -0,0 +1,15 @@
+//
+//  View.swift
+//  AlpineCore
+//
+//  Created by Jenya Lebid on 8/12/24.
+//
+
+import SwiftUI
+
+public extension View {
+    
+    var popouts: some View {
+        self.popoutPresenter
+    }
+}
