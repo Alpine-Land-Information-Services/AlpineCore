cc3a0dc13d8b3344bb5302206e64925bdefcd6b6
Jenya Lebid <61124137+jenyalebid@users.noreply.github.com>
2024-10-21 15:57:40 -0700

    Create ModelContext.swift

---
+++ Sources/AlpineCore/Extensions/Swift Data/ModelContext.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  AlpineCore
+//
+//  Created by Jenya Lebid on 10/21/24.
+//
+
+import Foundation
+import SwiftData
+
+public extension ModelContext {
+    
+    func find<Model: PersistentModel>(by predicate: Predicate<Model>) throws -> Model? {
+        var descriptor = FetchDescriptor(predicate: predicate)
+        descriptor.fetchLimit = 1
+        
+        return try self.fetch(descriptor).first
+    }
+    
+    func search<Model: PersistentModel>(by predicate: Predicate<Model>) throws -> [Model] {
+        let descriptor = FetchDescriptor(predicate: predicate)
+        return try self.fetch(descriptor)
+    }
+}
