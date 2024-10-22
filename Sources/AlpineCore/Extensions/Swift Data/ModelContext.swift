15abec242671ab8526d8b481924b266944502728
Jenya Lebid <61124137+jenyalebid@users.noreply.github.com>
2024-10-22 15:17:01 -0700

    Update ModelContext.swift

--- Sources/AlpineCore/Extensions/Swift Data/ModelContext.swift
+++ Sources/AlpineCore/Extensions/Swift Data/ModelContext.swift
@@ -17,7 +17,7 @@ public extension ModelContext {
         return try self.fetch(descriptor).first
     }
     
-    func search<Model: PersistentModel>(by predicate: Predicate<Model>) throws -> [Model] {
+    func search<Model: PersistentModel>(with predicate: Predicate<Model>) throws -> [Model] {
         let descriptor = FetchDescriptor(predicate: predicate)
         return try self.fetch(descriptor)
     }
